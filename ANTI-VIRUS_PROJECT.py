456086e481eb45ba75915339c0f7eb5110e710fc
tanmay232003 <tanmay22003@gmail.com>
2026-04-11 13:47:13 +0530

    ANTI-VIRUS_PROJECT.py

--- ANTI-VIRUS_PROJECT.py
+++ ANTI-VIRUS_PROJECT.py
@@ -1391,4 +1391,5 @@ def scan_stream():
 
 # -------------------- Run --------------------
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True, host='127.0.0.1', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, threaded=True, host='0.0.0.0', port=port)
